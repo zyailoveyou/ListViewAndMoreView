8e283f479dee8898dd955e6bfa7720c83f710006
zyailoveyou <296301136@qq.com>
2018-11-16 17:02:52 +0800

    今天全部

---
+++ app/src/main/java/com/cop/zqc/listviewdatebackup2/activity/ScrollViewActivtiy.java
@@ -0,0 +1,20 @@
+package com.cop.zqc.listviewdatebackup2.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cop.zqc.listviewdatebackup2.R;
+
+public class ScrollViewActivtiy extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.scrollview);
+
+
+
+
+    }
+}
