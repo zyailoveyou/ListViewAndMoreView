bba83a02720d7a47fa621d919e0e32583c4dbab8
zyailoveyou <296301136@qq.com>
2018-11-16 17:02:58 +0800

    今天全部

---
+++ app/src/main/java/com/cop/zqc/listviewdatebackup2/activity/UsingNotificationActivitya.java
@@ -0,0 +1,16 @@
+package com.cop.zqc.listviewdatebackup2.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cop.zqc.listviewdatebackup2.R;
+
+public class UsingNotificationActivitya extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.usingnotificationlayout);
+    }
+}
